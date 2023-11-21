824dc3bc142dab6b42222b2d23663c7ebb27977a
liniker06 <115920503+liniker06@users.noreply.github.com>
2023-11-21 17:32:43 +0300

    sprint 4

--- bookr/bookr/urls.py
+++ bookr/bookr/urls.py
@@ -1,10 +1,11 @@
 import reviews.views
-from django.contrib import admin
 from django.urls import include, path
+from django.contrib import admin
+
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', reviews.views.index),
-    path('book-search/', reviews.views.book_search),
-    path('', include('reviews.urls')),
+    path("admin/", admin.site.urls),
+    path("", reviews.views.index),
+    path("book-search/", reviews.views.book_search, name="book_search"),
+    path("", include("reviews.urls")),
 ]
